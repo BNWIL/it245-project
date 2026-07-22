373b1271fca1e2370134a05abdadea8cd6a5003d
BNWIL <3omar.jazza@gmail.com>
2026-07-22 03:01:03 +0300

    Create BookNode.java
    Create binary search tree node and class structure

---
+++ src/BookNode.java
@@ -0,0 +1,31 @@
+public class BookNode {
+    private Book book;
+    private BookNode left;
+    private BookNode right;
+
+    public BookNode(Book book) {
+        this.book = book;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public BookNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BookNode left) {
+        this.left = left;
+    }
+
+    public BookNode getRight() {
+        return right;
+    }
+
+    public void setRight(BookNode right) {
+        this.right = right;
+    }
+}
